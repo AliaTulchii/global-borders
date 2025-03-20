d32919266e7393fe49a881730de97a934de5c1a4
Halina Tulchii <halendeihalyna@gmail.com>
2025-03-20 22:08:43 +0200

    added styles for Mobile Btn

--- src/components/Btn/MobileNavBtn.tsx
+++ src/components/Btn/MobileNavBtn.tsx
@@ -1,19 +1,33 @@
 import { useState } from "react"
 import MobileNav from "../MobileNav";
 import { AnimatePresence } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 
 const MobileNavBtn = () => {
     const [isActive, setIsActive] = useState(false);
+    const location = useLocation();
+  
+    const isSpecialRoute = location.pathname === "/eu" || location.pathname === "/contacts";
+  
 
   return (
     <>
-    <div className={`mobile-nav__btn ${isActive ? "mobile-nav__btn-active" : ""}`}  onClick={() => setIsActive(!isActive)}>
-    <div className={`mobile-nav__burger ${isActive ? "burger-active" : ""}`}>
+    <div 
+        className={`mobile-nav__btn ${isActive ? "mobile-nav__btn-active" : ""}`}
+        onClick={() => setIsActive(!isActive)}
+        style={{
+            backgroundColor: isSpecialRoute || isActive ? "var(--accent)" : "var(--white)",
+        }}
+    >
+        <div
+         className={`mobile-nav__burger ${isActive ? "burger-active" : ""} ${isSpecialRoute ? "burger-page" : ""}`}
+         
+         ></div>
     </div>
-    </div>
+
     <AnimatePresence mode="wait">
-    {isActive && <MobileNav onClick={() => setIsActive(!isActive)}/>}
+        {isActive && <MobileNav onClick={() => setIsActive(false)} />}
     </AnimatePresence>
 </>
   )
